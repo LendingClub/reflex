612216fae4993417cae86688e60c303079f26db0
Rob Schoening <rschoening@lendingclub.com>
2016-12-29 14:39:25 -0800

    1.0.4

--- src/main/java/org/lendingclub/reflex/consumer/Consumers.java
+++ src/main/java/org/lendingclub/reflex/consumer/Consumers.java
@@ -3,6 +3,7 @@ package org.lendingclub.reflex.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.reactivex.Observer;
 import io.reactivex.functions.Consumer;
 
 public class Consumers {
@@ -20,6 +21,28 @@ public class Consumers {
 			public void accept(T t) throws Exception {
 				try {
 					consumer.accept(t);
+				} catch (Exception e) {
+					logger.error("exception in consumer", e);
+				}
+
+			}
+
+		};
+		return wrapper;
+	}
+	
+	public static <T> Consumer<T> safeConsumer(Observer<T> observer) {
+
+		return safeConsumer(observer, LoggerFactory.getLogger(observer.getClass()));
+	}
+	public static <T> Consumer<T> safeConsumer(Observer<T> observer, Logger logger) {
+
+		Consumer<T> wrapper = new Consumer<T>() {
+
+			@Override
+			public void accept(T t) throws Exception {
+				try {
+					observer.onNext(t);
 				} catch (Exception e) {
 					logger.error("exception in consumer", e);
 				}
--- src/main/java/org/lendingclub/reflex/queue/WorkQueue.java
+++ src/main/java/org/lendingclub/reflex/queue/WorkQueue.java
@@ -23,7 +23,9 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.subjects.PublishSubject;
 
 /**
- * WorkQueueObserver connects an Rx Observable to a bounded work queue and exposes the resulting work queue as an Observable.
+ * WorkQueueObserver connects an Rx Observable to a bounded work queue and
+ * exposes the resulting work queue as an Observable.
+ * 
  * @author rschoening
  *
  * @param <T>
@@ -33,8 +35,8 @@ public class WorkQueue<T> implements Observer<T> {
 	Logger logger = LoggerFactory.getLogger(WorkQueue.class);
 
 	static AtomicInteger count = new AtomicInteger(0);
-	
-	String name = "QueueSubscriber-"+(count.getAndIncrement())+"-%d";
+
+	String name = "QueueSubscriber-" + (count.getAndIncrement()) + "-%d";
 	int queueSize = 1000;
 	int coreThreadPoolSize = 1;
 	int maxThreadPoolSize = 1;
@@ -45,88 +47,91 @@ public class WorkQueue<T> implements Observer<T> {
 	ThreadPoolExecutor executor;
 	UncaughtExceptionHandler uncaughtExceptionHandler = null;
 	final PublishSubject<T> publishSubject = PublishSubject.create();
-	
-	
+
 	public WorkQueue() {
-		
+
 	}
 
 	class MyRejectedExecutionHandler extends ThreadPoolExecutor.DiscardPolicy {
 
 		@Override
 		public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
-			
+
 			super.rejectedExecution(r, e);
-			logger.error("rejected {} in {}",r,e);
+			logger.error("rejected {} in {}", r, e);
 		}
-		
+
 	};
-	
+
 	public ThreadPoolExecutor getThreadPoolExecutor() {
-		Preconditions.checkState(executor!=null,"WorkQueueObserver has not been started");
+		Preconditions.checkState(executor != null, "WorkQueueObserver has not been started");
 		return executor;
 	}
-	
+
 	public WorkQueue<T> withQueueSize(int size) {
 		assertNotStarted();
 		this.queueSize = size;
 		return this;
 	}
-	
+
 	public WorkQueue<T> withCoreThreadPoolSize(int size) {
 		assertNotStarted();
 		this.coreThreadPoolSize = size;
 		this.maxThreadPoolSize = Math.max(maxThreadPoolSize, this.coreThreadPoolSize);
 		return this;
 	}
+
 	public WorkQueue<T> withRejectedExecutionHandler(RejectedExecutionHandler handler) {
 		assertNotStarted();
 		this.rejectedExecutionHandler = handler;
 		return this;
 	}
+
 	public WorkQueue<T> withMaxThreadPoolSize(int size) {
 		assertNotStarted();
 		this.coreThreadPoolSize = size;
 		return this;
 	}
+
 	public WorkQueue<T> withThreadTimeout(int time, TimeUnit timeUnit) {
 		assertNotStarted();
 		this.time = time;
 		this.timeUnit = timeUnit;
 		return this;
 	}
-	
+
 	public WorkQueue<T> withThreadName(String name) {
 		assertNotStarted();
 		this.name = name;
 		return this;
 	}
-	
+
 	public Observable<T> getObservable() {
 		return publishSubject;
 	}
-	
+
 	protected synchronized void start() {
 		if (executor == null) {
 			queue = new LinkedBlockingDeque<>(queueSize);
-			
-			Preconditions.checkArgument(maxThreadPoolSize>=coreThreadPoolSize,"maxThreadPoolSize < coreThreadPoolSize");
-			
-			logger.info("Starting ThreadPoolExecutor for {}",this);
-			logger.info("coreSize: {}",coreThreadPoolSize);
-			logger.info("maxSize : {}",maxThreadPoolSize);
-			
-			
-			ThreadPoolExecutor localExecutor= new ThreadPoolExecutor(coreThreadPoolSize,maxThreadPoolSize, time, timeUnit,
+
+			Preconditions.checkArgument(maxThreadPoolSize >= coreThreadPoolSize,
+					"maxThreadPoolSize < coreThreadPoolSize");
+
+			logger.info("Starting ThreadPoolExecutor for {}", this);
+			logger.info("coreSize: {}", coreThreadPoolSize);
+			logger.info("maxSize : {}", maxThreadPoolSize);
+
+			ThreadPoolExecutor localExecutor = new ThreadPoolExecutor(coreThreadPoolSize, maxThreadPoolSize, time,
+					timeUnit,
 					queue);
 			ThreadFactoryBuilder tfb = new ThreadFactoryBuilder().setDaemon(true).setNameFormat(name);
-			if (uncaughtExceptionHandler!=null) {
+			if (uncaughtExceptionHandler != null) {
 				tfb.setUncaughtExceptionHandler(uncaughtExceptionHandler);
 			}
 			localExecutor.setThreadFactory(tfb.build());
-			
+
 			localExecutor.setRejectedExecutionHandler(rejectedExecutionHandler);
-			
+
 			logger.info("ThreadPoolExecutor started");
 			this.executor = localExecutor;
 		}
@@ -135,30 +140,32 @@ public class WorkQueue<T> implements Observer<T> {
 
 	@Override
 	public void onSubscribe(Disposable s) {
-		logger.info("onSubscribe({})" , s);
+		logger.info("onSubscribe({})", s);
 		start();
 	}
 
 	@Override
 	public void onNext(T t) {
-		
+
 		Runnable r = new Runnable() {
 
 			@Override
 			public void run() {
-		
+
 				publishSubject.onNext(t);
 			}
-			
+
 		};
-		
+
+	
 		executor.submit(r);
+		
 
 	}
 
 	@Override
 	public void onError(Throwable t) {
-		logger.debug("onError()",t);
+		logger.warn("onError()", t);
 
 	}
 
@@ -169,8 +176,7 @@ public class WorkQueue<T> implements Observer<T> {
 	}
 
 	private void assertNotStarted() {
-		Preconditions.checkState(executor==null,"start() called already");
+		Preconditions.checkState(executor == null, "start() called already");
 	}
-	
 
 }
