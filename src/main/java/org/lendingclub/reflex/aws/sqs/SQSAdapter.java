be77354f630b6d6ab800ec93ec0107c018fb6176
Rob Schoening <rschoening@lendingclub.com>
2016-12-30 06:55:48 -0800

    major sqs fixes

--- src/main/java/org/lendingclub/reflex/aws/sqs/SQSAdapter.java
+++ src/main/java/org/lendingclub/reflex/aws/sqs/SQSAdapter.java
@@ -1,9 +1,11 @@
 package org.lendingclub.reflex.aws.sqs;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,10 @@ import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.util.concurrent.UncaughtExceptionHandlers;
 
 import io.reactivex.Observable;
 import io.reactivex.functions.Function;
@@ -36,15 +42,46 @@ public class SQSAdapter {
 		public SQSAdapter getSQSAdapter() {
 			return SQSAdapter.this;
 		}
+
 		public void delete() {
 			SQSAdapter.this.delete(message);
+		}
+	}
+	
+	public static class SQSUrlSupplier implements Supplier<String> {
+
+		private String queueUrl;
+		private AmazonSQSClient client;
+		private String queueName;
+
+		public SQSUrlSupplier(String url) {
+			this.queueUrl = url;
+		}
+
+		public SQSUrlSupplier(AmazonSQSClient client, String queueName) {
+			this.client = client;
+			this.queueName = queueName;
+		}
+
+		@Override
+		public String get() {
+			if (!Strings.isNullOrEmpty(queueUrl)) {
+				return queueUrl;
+			} else if (client != null && queueName != null) {
+				logger.info("resolving url for queue: {}", queueName);
+
+				GetQueueUrlResult result = client.getQueueUrl(queueName);
+
+				return result.getQueueUrl();
+			}
+			throw new IllegalArgumentException("must provide explicit url or a client+queueName");
 		}
 	}
 
 	static Logger logger = LoggerFactory.getLogger(SQSAdapter.class);
 
 	PublishSubject<SQSMessage> publishSubject = PublishSubject.create();
-	String queueUrl;
+	Supplier<String> urlSupplier;
 	AmazonSQSClient sqs;
 	String queueName;
 	boolean autoDelete = true;
@@ -53,17 +90,21 @@ public class SQSAdapter {
 	AtomicBoolean running = new AtomicBoolean(false);
 
 	long backoffMutiplierMillis = 100;
-	long backoffMaxMillis= TimeUnit.SECONDS.toMillis(60);
+	long backoffMaxMillis = TimeUnit.SECONDS.toMillis(60);
 	AtomicLong failureCount = new AtomicLong(0);
 
+
 	public SQSAdapter withSQSClient(AmazonSQSClient client) {
+
 		this.sqs = client;
 		return this;
 	}
 
 	/**
-	 * SQS is implemented with long-polling.  This is the number of seconds that the SQS receive-message operation will block
-	 * before closing and executing another.  Default: 10
+	 * SQS is implemented with long-polling. This is the number of seconds that
+	 * the SQS receive-message operation will block before closing and executing
+	 * another. Default: 10
+	 * 
 	 * @param secs
 	 * @return
 	 */
@@ -73,7 +114,9 @@ public class SQSAdapter {
 	}
 
 	/**
-	 * The SQS client can request between 1-10 messages per GET request.  Default: 10.
+	 * The SQS client can request between 1-10 messages per GET request.
+	 * Default: 10.
+	 * 
 	 * @param count
 	 * @return
 	 */
@@ -83,8 +126,10 @@ public class SQSAdapter {
 	}
 
 	/**
-	 * If you do not specify the queue URL and the client is set to the correct region, SQSAdapter will resolve the URL
-	 * from the unqualified queue name.  If the url is set, there is no need to set the name.
+	 * If you do not specify the queue URL and the client is set to the correct
+	 * region, SQSAdapter will resolve the URL from the unqualified queue name.
+	 * If the url is set, there is no need to set the name.
+	 * 
 	 * @param name
 	 * @return
 	 */
@@ -92,9 +137,10 @@ public class SQSAdapter {
 		this.queueName = name;
 		return this;
 	}
-	
+
 	/**
-	 * Exponential backoff multiplier.  (failureCount ^ 2 * multiplier)
+	 * Exponential backoff multiplier. (failureCount ^ 2 * multiplier)
+	 * 
 	 * @param millis
 	 * @return
 	 */
@@ -103,19 +149,21 @@ public class SQSAdapter {
 		this.backoffMutiplierMillis = millis;
 		return this;
 	}
-	
+
 	public boolean isAutoDeleteEnabled() {
 		return autoDelete;
 	}
-	
+
 	public SQSAdapter withAutoDeleteEnabled(boolean b) {
 		assertNotStarted();
 		this.autoDelete = b;
 		return this;
 	}
+
 	/**
-	 * Maximum number of milliseconds that retries will wait in the case of failure.
-	 * Default: 60000ms (60 seconds)
+	 * Maximum number of milliseconds that retries will wait in the case of
+	 * failure. Default: 60000ms (60 seconds)
+	 * 
 	 * @param millis
 	 * @return
 	 */
@@ -124,15 +172,16 @@ public class SQSAdapter {
 		this.backoffMaxMillis = millis;
 		return this;
 	}
-	
+
 	/**
 	 * Sets the SQS queue URL that the SQSAdpater will use for polling.
+	 * 
 	 * @param url
 	 * @return
 	 */
 	public SQSAdapter withQueueUrl(String url) {
 		assertNotStarted();
-		this.queueUrl = url;
+		this.urlSupplier = new SQSUrlSupplier(url);
 		return this;
 	}
 
@@ -148,7 +197,7 @@ public class SQSAdapter {
 				}
 				return Observable.just(n);
 			} catch (Exception e) {
-				logger.warn("problem parsing message from queue: "+t.getSQSAdapter().getQueueUrl(), e);
+				logger.warn("problem parsing message from queue: " + t.getSQSAdapter().getQueueUrl(), e);
 			}
 			return Observable.empty();
 		}
@@ -156,28 +205,50 @@ public class SQSAdapter {
 	}
 
 	public String getQueueUrl() {
-		return queueUrl;
+		if (urlSupplier == null) {
+			if ((!Strings.isNullOrEmpty(queueName)) && sqs != null) {
+				urlSupplier = Suppliers.memoize(new SQSUrlSupplier(sqs, queueName));
+			} else {
+				return null;
+			}
+		}
+		return urlSupplier.get();
+	}
+
+	public synchronized void stop() {
+		logger.info("stopping....");
+		running.set(false);
 	}
 	public synchronized SQSAdapter start() {
 		if (running.get()) {
-			throw new IllegalStateException("already running");
+			logger.warn("already running");
+			return this;
 		}
-		if (queueUrl == null && queueName != null) {
-			GetQueueUrlResult getQueueUrlResult = this.sqs.getQueueUrl(queueName);
-			queueUrl = getQueueUrlResult.getQueueUrl();
+
+		Preconditions.checkArgument(sqs != null, "SQSClient must be set");
+		if (urlSupplier == null && queueName != null) {
+			urlSupplier = Suppliers.memoize(new SQSUrlSupplier(sqs, queueName));
+		}
+
+		if (urlSupplier == null) {
+			throw new IllegalArgumentException("queueUrl or queueName must be set");
 		}
 
 		Runnable r = new Runnable() {
 
 			public void run() {
 
-				try {
-					running.set(true);
-					while (running.get()) {
+				running.set(true);
+				while (running.get()) {
+					try {
 						ReceiveMessageRequest rmr = new ReceiveMessageRequest();
 						rmr.setWaitTimeSeconds(waitTimeSeconds);
 						rmr.setMaxNumberOfMessages(messagesPerRequest);
-						rmr.setQueueUrl(queueUrl);
+						if (urlSupplier == null) {
+							throw new IllegalArgumentException("queueUrl or queueName must be set");
+						}
+
+						rmr.setQueueUrl(urlSupplier.get());
 						ReceiveMessageResult result = sqs.receiveMessage(rmr);
 						List<Message> list = result.getMessages();
 
@@ -197,11 +268,12 @@ public class SQSAdapter {
 								}
 							}
 						}
-					}
-				} catch (Exception e) {
-					handleException(e);
-				}
 
+					} catch (Exception e) {
+						handleException(e);
+					}
+				}
+				logger.info("stopped");
 			}
 		};
 
@@ -215,32 +287,52 @@ public class SQSAdapter {
 	}
 
 	public void delete(Message m) {
-		logger.info("deleting {} {}", queueUrl, m.getReceiptHandle());
-		DeleteMessageRequest dmr = new DeleteMessageRequest(queueUrl, m.getReceiptHandle());
+		String url = getQueueUrl();
+		logger.info("deleting {} {}", url, m.getReceiptHandle());
+		DeleteMessageRequest dmr = new DeleteMessageRequest(url, m.getReceiptHandle());
 		sqs.deleteMessage(dmr);
 	}
 
-	
 	private void resetFailureCount() {
 		failureCount.set(0);
 	}
-	private void assertNotStarted() {
-		Preconditions.checkState(running.get()==false,"start() already called");
-	}
-	protected void handleException(Exception e) {
-		logger.warn("problem receiving message from queue: " + queueUrl, e);
-		long count = failureCount.incrementAndGet();
 
-		long wait = (count ^ 2) * 100;
-		wait = Math.min(wait, TimeUnit.SECONDS.toMillis(60));
+	private void assertNotStarted() {
+		Preconditions.checkState(running.get() == false, "start() already called");
+	}
+
+	protected void handleException(Exception e) {
+
+	
+		
 		try {
-			logger.debug("sleeping for {}ms due to {} failures",wait,count);
-			Thread.sleep(wait);
+			String url = getQueueUrl();
+			logger.warn("problem receiving message from queue: " + url, e);
+		} catch (RuntimeException e2) {
+			logger.warn("problem receiving message from queue: " + queueName, e2);
+
+		}
+
+		
+
+		try {
+			
+			logger.info("sleeping for {}ms due to {} failures", getBackoffInterval(), failureCount.get());
+			Thread.sleep(getBackoffInterval());
 		} catch (InterruptedException x) {
 		}
+		logger.info("continuing");
+
 	}
 
 	public Observable<SQSMessage> getObservable() {
 		return publishSubject;
+	}
+	
+	protected long getBackoffInterval() {
+
+		return Math.min(Math.round(Math.pow(2, failureCount.getAndIncrement())) * backoffMutiplierMillis, TimeUnit.SECONDS.toMillis(60));
+		
+	
 	}
 }
--- src/test/java/org/lendingclub/reflex/aws/sqs/SQSAdapterTest.java
+++ src/test/java/org/lendingclub/reflex/aws/sqs/SQSAdapterTest.java
@@ -2,6 +2,11 @@ package org.lendingclub.reflex.aws.sqs;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 
 public class SQSAdapterTest {
 
@@ -12,6 +17,50 @@ public class SQSAdapterTest {
 		SQSAdapter adapter = new SQSAdapter();
 		Assertions.assertThat(adapter.isAutoDeleteEnabled()).isTrue();
 		Assertions.assertThat(adapter.backoffMaxMillis).isEqualTo(60000);
+	}
+	
+	@Test
+	public void testMockQueueLookup()  {
+		AmazonSQSClient client = Mockito.mock(AmazonSQSClient.class);
+		
+		GetQueueUrlResult result = new GetQueueUrlResult();
+		result.setQueueUrl("https://sqs.us-west-2.amazonaws.com/123456789012/foo");
+		Mockito.when(client.getQueueUrl("foo")).thenReturn(result);
+		SQSAdapter adapter = new SQSAdapter()
+				.withQueueName("foo")
+				.withSQSClient(client);
+		
+		Assertions.assertThat(adapter.getQueueUrl()).isEqualTo(result.getQueueUrl());
+	
+	}
+	
+	@Test
+	public void testLazyFailure()  throws InterruptedException {
+	
+		AmazonSQSClient client = new AmazonSQSClient(new BasicAWSCredentials("foo", "bar"));
+		
+		SQSAdapter adapter = new SQSAdapter()
+				.withQueueName("foo")
+				.withSQSClient(client);
+		
+		adapter.start();		
+		Thread.sleep(10000);
+		// What we are checking for is that the adapters started running *AND* continued to run
+		Assertions.assertThat(adapter.failureCount.get()).isGreaterThanOrEqualTo(2);
+		
+		adapter.stop();
+	}
+	
+	@Test
+	public void testBackoff() {
+		SQSAdapter adpter = new SQSAdapter();
+		Assertions.assertThat(adpter.failureCount.get()).isEqualTo(0);
+		Assertions.assertThat(adpter.getBackoffInterval()).isEqualTo(adpter.backoffMutiplierMillis);
+		adpter.failureCount.set(5);
+		Assertions.assertThat(adpter.getBackoffInterval()).isEqualTo(3200);
+		
+		adpter.failureCount.set(50);
+		Assertions.assertThat(adpter.getBackoffInterval()).isEqualTo(adpter.backoffMaxMillis);
 	}
 
 }
